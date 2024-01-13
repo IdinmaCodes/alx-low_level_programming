e1e08cd1d0c2932b85bd10dd79134a640199472c
Idinma <idigweamaka@gmail.com>
2024-01-13 09:13:10 +0100

    correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,13 +16,13 @@ if (n > 5)
 {
 printf("%d is and is greater than 5\n", n);
 }
-if (n == 0)
+else if (n < 0)
 {
-printf("%d and is 0\n", n);
+printf("%d and is less than 6 and not 0\n", n);
 }
 else
 {
-printf("%d and is less than 6 and not 0\n", n);
+printf("%d and is 0\n", n);
 }
 return (0);
 }
