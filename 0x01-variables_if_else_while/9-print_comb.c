f6d4110fb5ca009c29c807ae32ad7d4de8417172
Idinma <idigweamaka@gmail.com>
2024-02-07 14:02:37 +0100

    correction

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,27 +1,22 @@
 #include <stdio.h>
-
 /**
- * main - program that prints all possible combinations of single-digit
- *            numbers separated by commas and spaces
- *
- * Return: 0 on success
- */
-
+* main - program that prints all possible combinations of single-digit
+*            numbers separated by commas and spaces
+*
+* Return: 0 on success
+*/
 int main(void)
 {
-    int digit;
-
-    for (digit = 0; digit <= 9; digit++)
-    {
-        putchar(digit + '0');
-
-        if (digit < 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
-
-    putchar('\n');
-    return (0);
+int digit;
+for (digit = 0; digit <= 9; digit++)
+{
+putchar(digit + '0');
+if (digit != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
