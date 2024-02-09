2d6c6ab7a9e93679f0e5c5d7b8d2778abfd8e6ab
Idinma <idigweamaka@gmail.com>
2024-02-09 23:44:39 +0100

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,21 @@
 #include <stdio.h>
 #include "main.h"
-/**
-* print_sign - unction that prints the sign of a numbe
-* @n: character to be checked
-*
-* Return: 1 if positive, 0 if zero, -1 if negative
-*/
+
 int print_sign(int n)
 {
-if (n > 0)
-{
-putchar('+');
-return (1);
-}
-else if (n == 0)
-{
-putchar('0');
-return (0);
-}
-else
-{
-putchar('_');
-return (-1);
-}
+    if (n > 0)
+    {
+     putchar('+');
+     return (1);
+    }
+    else if (n == 0)
+    {
+        putchar('0');
+        return (0);
+    }
+    else
+    {
+        putchar('-');
+        return (-1);
+    }
 }
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - function that prints the last digit of a number
+ *
+ * Returns the value of the last digit
+ */
+
+int print_last_digit(int)
+{
+    if (n >= 0)
+    last-digit = n % 10
+    {
+     putchar(last_digit);
+    }
+    else
+    last_digit = -n % 10
+    {
+     putchar(last_digit);
+    }
+}
