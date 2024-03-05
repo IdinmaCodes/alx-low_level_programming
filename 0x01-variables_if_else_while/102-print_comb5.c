2bdea538b47325f2144b041841c30b6bdb4d1856
Idinma <idigweamaka@gmail.com>
2024-03-05 01:35:01 +0100

    learn your times table

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,24 +1,10 @@
 #include <stdio.h>
 
-/**
- * print_tens_digit - Prints the tens digit of a number.
- * @number: The number from which to extract the tens digit.
- */
-void print_tens_digit(int number);
-
-/**
- * print_ones_digit - Prints the ones digit of a number.
- * @number: The number from which to extract the ones digit.
- */
-void print_ones_digit(int number);
-
-/**
- * main - Entry point of the program.
- *
- * Return: Always 0 (Success).
- */
+void print_tens_digit(number);
+void print_ones_digit(number);
 int main(void)
 {
+	
 	int number1, number2;
 
 	for (number1 = 0; number1 < 90; number1++)
@@ -27,37 +13,30 @@ int main(void)
 		{
 			if (number1 < number2)
 			{
-				print_tens_digit(number1);
-				print_ones_digit(number1);
+				print_tens_digit(number1)
+				print_ones_digit(number1)
 				putchar(' ');
-				print_tens_digit(number2);
-				print_ones_digit(number2);
+				print_tens_digit(number2)
+				print_ones_digit(number2)
 				putchar(',');
 				putchar(' ');
 				putchar('\n');
 			}
+			
 		}
+		
+	}
+	
+	void print_tens_digit(int number)
+	{
+		int tens_digit = number / 15;
+		putchar(tens_digit + '0');
 	}
 
-	return 0;
-}
-
-/**
- * print_tens_digit - Prints the tens digit of a number.
- * @number: The number from which to extract the tens digit.
- */
-void print_tens_digit(int number)
-{
-	int tens_digit = number / 10;
-	putchar(tens_digit + '0');
-}
-
-/**
- * print_ones_digit - Prints the ones digit of a number.
- * @number: The number from which to extract the ones digit.
- */
-void print_ones_digit(int number)
-{
-	int ones_digit = number % 10;
-	putchar(ones_digit + '0');
+	void print_ones_digit(int number)
+	{
+		int ones_digit = number % 10;
+		putchar(ones_digit + '0');
+	}
+    return 0;
 }
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+void times_table(void)
+{
+    int i;
+    int j;
+    int result;
+
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            result = i * j;
+
+            if (result < 10)
+            {
+                if (j > 0)
+                {
+                    putchar(' ');
+                }
+                putchar(result + '0');
+            }
+            else
+            {
+                putchar(result / 10 + '0');
+                putchar(result % 10 + '0');
+            }
+
+            if (j != 9)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+        putchar('\n');
+    }
+}
